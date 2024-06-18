67e56d05032ff3ec983add9c370a401130470b35
Rouven Himmelstein <rouven@himmelstein.info>
2024-06-18 20:00:15 +0200

    feat: add tauri

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,69 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
+use tauri::Manager;
+
+use crate::cpu_test::CpuTestResponse;
+
+mod cpu_test;
+mod mprime;
+mod process;
+mod ycruncher;
+
+pub struct AppState {
+    pub test_results: Arc<Mutex<HashMap<usize, CpuTestResponse>>>,
+}
+
+fn main() {
+    tauri::Builder::default()
+        .manage(AppState {
+            test_results: Arc::new(Mutex::new(HashMap::new())),
+        })
+        .setup(|app| {
+            let title = format!("PBO Assistant {}", env!("CARGO_PKG_VERSION"));
+            app.get_window("main").unwrap().set_title(&title).unwrap();
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![start_test])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+#[tauri::command]
+async fn start_test() -> Result<(), String> {
+    let core_to_test = "";
+
+    let cores_to_test = cpu_test::get_cores_to_test(core_to_test, cpu_test::get_physical_cores());
+
+    let config = cpu_test::CpuTestConfig {
+        duration_per_core: "20s".to_string(),
+        cores_to_test,
+        test_methods: vec![
+            cpu_test::CpuTestMethod::Prime95,
+            cpu_test::CpuTestMethod::YCruncher,
+        ],
+    };
+
+    // Holds the global state of the test results
+    let test_results: Arc<Mutex<HashMap<usize, CpuTestResponse>>> =
+        Arc::new(Mutex::new(HashMap::new()));
+
+    cpu_test::initialize_response(&config, &test_results, &config.duration_per_core);
+
+    cpu_test::run(config, test_results.clone());
+
+    let rest_results = test_results.lock().unwrap();
+    let mut values: Vec<&CpuTestResponse> = rest_results.values().collect();
+    values.sort_by(|a, b| a.core_id.cmp(&b.core_id));
+    println!();
+    println!();
+    println!();
+    for cpu_result in values {
+        println!("{:?}", cpu_result);
+    }
+
+    Ok(())
+}
---
+++ src-tauri/src/ycruncher.rs
@@ -0,0 +1,78 @@
+use flate2::read::GzDecoder;
+use std::io::{Cursor, Write};
+use std::process::{Child, Command};
+use std::thread;
+use std::time::Duration;
+use tar::Archive;
+
+pub const ERROR_MESSAGE: &str = "Errors encountered.";
+const PROCESS_PATH: &str = "/tmp/pbo-assistant/ycruncher/y-cruncher";
+
+pub fn initialize() {
+    let memory_file = include_bytes!("../../assets/ycruncher/ycruncher.tar.gz");
+
+    std::fs::create_dir_all("/tmp/pbo-assistant/ycruncher").expect("Failed to create directory");
+
+    let cursor = Cursor::new(memory_file);
+    let gz = GzDecoder::new(cursor);
+    let mut archive = Archive::new(gz);
+
+    let output_dir = "/tmp/pbo-assistant/";
+    archive.unpack(output_dir).expect("Failed to unpack");
+
+    Command::new("chmod")
+        .arg("+x")
+        .arg(PROCESS_PATH)
+        .output()
+        .expect("Failed to change permissions");
+}
+
+fn spawn_process(core_id: usize) -> Child {
+    let logical_core_id = core_id * 2;
+
+    let mut child_process = Command::new(PROCESS_PATH)
+        .stdin(std::process::Stdio::piped())
+        .stdout(std::process::Stdio::piped())
+        .stderr(std::process::Stdio::piped())
+        .spawn()
+        .expect("Failed to start the process");
+
+    let mut stdin = child_process.stdin.take().unwrap();
+
+    // Apply this inputs:
+    // 1   Component Stress Tester
+    // 1   Modify Cores
+    // d   Disable all Cores
+    // #   Number of logical core id
+    // \n  confirm core dialog
+    // 2   Modify memory settins
+    // \n  confirm core dialog
+    // 5   Run Forever (we are managing the time)
+    // 0   Start Stress test
+    stdin.write_all(b"1\n").unwrap();
+    stdin.write_all(b"1\n").unwrap();
+    stdin.write_all(b"d\n").unwrap();
+    stdin
+        .write_all(format!("{}\n", logical_core_id).as_bytes())
+        .unwrap();
+    stdin.write_all(b"\n").unwrap();
+    stdin.write_all(b"2\n").unwrap();
+    stdin.write_all(b"\n").unwrap();
+    stdin.write_all(b"5\n").unwrap();
+    stdin.write_all(b"0\n").unwrap();
+
+    let proccess_id = child_process.id();
+
+    println!("Started process with id: {}", proccess_id);
+
+    child_process
+}
+
+pub fn start_verification(core_id: usize) -> Child {
+    let child = spawn_process(core_id);
+
+    // Wait a second to make sure the process is started
+    thread::sleep(Duration::from_secs(1));
+
+    child
+}

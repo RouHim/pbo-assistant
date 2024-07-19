6e19ed14760153a24e167828b4d79be8cc4e0e9e
Rouven Himmelstein <r.himmelstein@tarent.de>
2024-07-19 09:28:10 +0200

    feat: improve ui

--- src/main.js
+++ src/main.js
@@ -12,22 +12,7 @@ window.addEventListener("DOMContentLoaded", () => {
     loadTestMethods();
 
     startButton.addEventListener("click", () => onStartTestButtonClick());
-
-    testDebugShowCores();
 });
-
-function testDebugShowCores() {
-    invoke("get_physical_cores").then((core_count) => {
-        const cpusLayout = document.getElementById("cpusLayout");
-        for (let i = 0; i < core_count; i++) {
-            const div = document.createElement("div");
-            div.id = `cpu${i}`;
-            div.className = "cpuLayout";
-            div.appendChild(document.createTextNode("# " + i));
-            cpusLayout.appendChild(div);
-        }
-    });
-}
 
 function onStartTestButtonClick() {
     if (isTestRunning) {
@@ -78,91 +63,114 @@ function updateCpuStatus(cpuTestStatus) {
 
     // If it doesn't exist, create it
     if (!cpuLayout) {
-        const cpusLayout = document.getElementById("cpusLayout");
-        const div = document.createElement("div");
-        div.id = `cpu${cpuTestStatus.core_id}`;
-        div.className = "cpuLayout";
-        cpusLayout.appendChild(div);
-        cpuLayout = div;
-
-        // Core id
-        cpuLayout.appendChild(document.createTextNode(`# ${cpuTestStatus.core_id}`));
-
-        // Create and set current test method status
-        const methodsDiv = document.createElement("div");
-        methodsDiv.id = `${cpuTestStatus.core_id}MethodLayout`;
-        const methods = cpuTestStatus.method_response;
-        for (const method in methods) {
-            const methodDiv = document.createElement("div");
-            const methodStatus = methods[method];
-            methodDiv.id = `${cpuTestStatus.core_id}${method}Status`;
-            methodDiv.innerText = `${method}: ${methodStatus.state}`;
-            methodsDiv.appendChild(methodDiv);
-        }
-        cpuLayout.appendChild(methodsDiv);
-
-        // Create and set clock speed
-        const clockDiv = document.createElement("div");
-        clockDiv.id = `${cpuTestStatus.core_id}Clock`;
-        clockDiv.innerText = `Clock: ${cpuTestStatus.max_clock} MHz`;
-        cpuLayout.appendChild(clockDiv);
-
-        // Create and set verification status
-        const verificationDiv = document.createElement("div");
-        verificationDiv.id = `${cpuTestStatus.core_id}Verification`;
-        verificationDiv.innerText = `Verification: ${cpuTestStatus.verification_failed ? "Failed" : "Passed"}`;
-        cpuLayout.appendChild(verificationDiv);
-
-        // Create and set progress
-        const progressDiv = document.createElement("div");
-        progressDiv.id = `${cpuTestStatus.core_id}Progress`;
-        progressDiv.style.display = "none";
-        cpuLayout.appendChild(progressDiv);
-
-        // Create and set progress bar
-        const progressBar = document.createElement("progress");
-        progressBar.id = `${cpuTestStatus.core_id}ProgressBar`;
-        progressBar.style.display = "none";
-        cpuLayout.appendChild(progressBar);
+        createCpuStatusLayout(cpuTestStatus, cpuLayout);
     } else {
         // The CPU layout already exists, just update the according values
-
-        // Update current status
-        const methods = cpuTestStatus.method_response;
-        for (const method in methods) {
-            let methodStatusTextNode = document.getElementById(`${cpuTestStatus.core_id}${method}Status`);
-            const methodStatus = methods[method];
-            methodStatusTextNode.innerText = `${method}: ${methodStatus.state}`;
-        }
-
-        // Update clock speed
-        const maxClockTextNode = document.getElementById(`${cpuTestStatus.core_id}Clock`);
-        maxClockTextNode.innerText = `Clock: ${cpuTestStatus.max_clock} MHz`;
-
-        // Update verification status
-        const cpuTestStatusTextNode = document.getElementById(`${cpuTestStatus.core_id}Verification`);
-        cpuTestStatusTextNode.innerText = `Verification: ${cpuTestStatus.verification_failed ? "Failed" : "Passed"}`;
-
-        // Update the progress
-        let anyMethodActive = Object.values(methods).some((method) => method.state === "Testing");
-        let currentActiveMethod = Object.values(methods).find((method) => method.state === "Testing");
-
-        // Update progress text
-        const progressTextNode = document.getElementById(`${cpuTestStatus.core_id}Progress`);
-        progressTextNode.style.display = anyMethodActive ? "block" : "none";
-        if (anyMethodActive) {
-            progressTextNode.innerText = `Progress: ${currentActiveMethod.current_secs}/${currentActiveMethod.total_secs}`;
-        }
-
-        // Update progress bar
-        const progressBar = document.getElementById(`${cpuTestStatus.core_id}ProgressBar`);
-        progressBar.style.display = anyMethodActive ? "block" : "none";
-        if (anyMethodActive) {
-            progressBar.max = currentActiveMethod.total_secs;
-            progressBar.value = currentActiveMethod.current_secs;
-        }
+        updateCpuStatusLayout(cpuTestStatus, cpuLayout);
     }
 
+}
+
+function createCpuStatusLayout(cpuTestStatus, cpuLayout) {
+    const cpusLayout = document.getElementById("cpusLayout");
+    const div = document.createElement("div");
+    div.id = `cpu${cpuTestStatus.core_id}`;
+    div.className = "cpuLayout";
+    cpusLayout.appendChild(div);
+    cpuLayout = div;
+
+    // Core id
+    cpuLayout.appendChild(document.createTextNode(`# ${cpuTestStatus.core_id}`));
+
+    // Create and set current test method status
+    const methodsDiv = document.createElement("div");
+    methodsDiv.id = `${cpuTestStatus.core_id}MethodLayout`;
+    const methods = cpuTestStatus.method_response;
+    for (const method in methods) {
+        const methodDiv = document.createElement("div");
+        const methodStatus = methods[method];
+        methodDiv.id = `${cpuTestStatus.core_id}${method}Status`;
+        methodDiv.innerText = `${method}: ${methodStatus.state}`;
+        methodsDiv.appendChild(methodDiv);
+    }
+    cpuLayout.appendChild(methodsDiv);
+
+    // Create and set clock speed
+    const clockDiv = document.createElement("div");
+    clockDiv.id = `${cpuTestStatus.core_id}Clock`;
+    clockDiv.innerText = `Max. Clock: ${cpuTestStatus.max_clock} MHz`;
+    cpuLayout.appendChild(clockDiv);
+
+    // Create and set progress
+    const progressDiv = document.createElement("div");
+    progressDiv.id = `${cpuTestStatus.core_id}Progress`;
+    progressDiv.style.display = "none";
+    cpuLayout.appendChild(progressDiv);
+
+    // Create and set progress bar
+    const progressBar = document.createElement("progress");
+    progressBar.id = `${cpuTestStatus.core_id}ProgressBar`;
+    progressBar.style.display = "none";
+    cpuLayout.appendChild(progressBar);
+}
+
+function updateCpuStatusLayout(cpuTestStatus, cpuLayout) {
+    const methods = cpuTestStatus.method_response;
+
+    // Determine cpu states
+    let isAllMethodsIdle = Object.values(methods).every((method) => method.state === "Idle");
+    let isAnyMethodTesting = Object.values(methods).some((method) => method.state === "Testing");
+    let isAllMethodsSuccess = Object.values(methods).every((method) => method.state === "Success");
+    let isAnyMethodFailed = Object.values(methods).some((method) => method.state === "Failed");
+    let isAnyIdleAndAnySuccess = Object.values(methods).some((method) => method.state === "Idle") && Object.values(methods).some((method) => method.state === "Success");
+
+    // Update current status
+    for (const method in methods) {
+        let methodStatusTextNode = document.getElementById(`${cpuTestStatus.core_id}${method}Status`);
+        const methodStatus = methods[method];
+        methodStatusTextNode.innerText = `${method}: ${methodStatus.state}`;
+    }
+
+    // Update clock speed
+    const maxClockTextNode = document.getElementById(`${cpuTestStatus.core_id}Clock`);
+    maxClockTextNode.innerText = `Max. Clock: ${cpuTestStatus.max_clock} MHz`;
+
+    // Update the progress
+    let currentMethodInTesting = Object.values(methods).find((method) => method.state === "Testing");
+
+    // Update progress text
+    const progressTextNode = document.getElementById(`${cpuTestStatus.core_id}Progress`);
+    progressTextNode.style.display = isAnyMethodTesting ? "block" : "none";
+    if (isAnyMethodTesting) {
+        progressTextNode.innerText = `Progress: ${currentMethodInTesting.current_secs}/${currentMethodInTesting.total_secs}`;
+    }
+
+    // Update progress bar
+    const progressBar = document.getElementById(`${cpuTestStatus.core_id}ProgressBar`);
+    progressBar.style.display = isAnyMethodTesting ? "block" : "none";
+    if (isAnyMethodTesting) {
+        progressBar.max = currentMethodInTesting.total_secs;
+        progressBar.value = currentMethodInTesting.current_secs;
+    }
+
+    // Update verification status
+    // Set style classes of cpu div accordingly the current state ( Idle,  Testing, Success, Failed,):
+    // if:
+    // - All methods in idle state -> set idle style class
+    // - Any method is in testing state -> set testing style class
+    // - All methods in success state -> set success style class
+    // - Any method in failed state -> set failed style class
+    let className = "cpuLayout";
+    if (isAllMethodsIdle) {
+        className += " cpu-idle";
+    } else if (isAnyMethodTesting || isAnyIdleAndAnySuccess) {
+        className += " cpu-testing";
+    } else if (isAllMethodsSuccess) {
+        className += " cpu-success";
+    } else if (isAnyMethodFailed) {
+        className += " cpu-failed";
+    }
+    cpuLayout.className = className;
 }
 
 function startStatusPolling() {
